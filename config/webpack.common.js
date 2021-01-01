7e81a9263f66b47653ab2b8062f5fa2967c4cad5
anggifergian <anggyfergian56@gmail.com>
2021-01-01 21:59:23 +0700

    Add loaders: babel-loader, css-loader, style-loader, sass-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,4 +14,19 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 }
