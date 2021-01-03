daed934f27f0cf9a8c0cff3537333c4e53d7d46f
anggifergian <anggyfergian56@gmail.com>
2021-01-03 22:11:35 +0700

    Add react-router-dom: add route in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <React.Fragment>
+      <h1>App.js</h1>
+      <Switch>
+        <Route path="/profile" component={Profile} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/home" component={Home} />
+        <Redirect from="/" exact to="/home" />
+      </Switch>
+    </React.Fragment>
+  );
 };
 
 export default App;
